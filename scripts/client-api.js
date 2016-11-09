a4dc114a524c48a7c402ebc48c824258f902fe7d
nowido <nowido@mail.ru>
2016-11-09 21:32:27 +0300

    Update client-api.js

--- scripts/client-api.js
+++ scripts/client-api.js
@@ -19,6 +19,17 @@ function generateUniqueKey(length)
     
     return s;
 }
+
+//----------------------------------------------------------------------------- 
+
+function decimalRound(number, precision) 
+{
+    var factor = Math.pow(10, precision);
+    var tempNumber = number * factor;
+    var roundedTempNumber = Math.round(tempNumber);
+    return roundedTempNumber / factor;
+};
+
 //----------------------------------------------------------------------------- 
 function AsyncCommander(socket, channel)
 {
